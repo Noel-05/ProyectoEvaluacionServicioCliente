b619cde3a94d6db28f17e443f6a48480ef5832bc
blackterminator7 <blackterminatormaster2@gmail.com>
2021-05-24 17:55:59 -0600

    Reseteo de Contraseña

--- SistemaACOPUS/settings.py
+++ SistemaACOPUS/settings.py
@@ -136,3 +136,11 @@ LOGIN_REDIRECT_URL = reverse_lazy('evaluacioncliente:index')
 IMPORT_EXPORT_USE_TRANSACTIONS = True 
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True 
+
+#Recuperar Contraseña
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'recuperacionacopus@gmail.com'
+EMAIL_HOST_PASSWORD = 'RecuperacionAcopus.1'
--- SistemaACOPUS/urls.py
+++ SistemaACOPUS/urls.py
@@ -15,10 +15,38 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.urls import reverse_lazy
+from django.contrib.auth import views as auth_views
+from .forms import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.proyecto.urls')),
     path('',include('apps.usuario.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"),
+         name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_form.html", form_class=FormularioContraOlvidada), 
+        name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_done.html"), 
+        name="password_reset_complete"),
+
 ]
+
+'''
+1 - Submit email form                         //PasswordResetView.as_view()
+2 - Email sent success message                //PasswordResetDoneView.as_view()
+3 - Link to password Rest form in email       //PasswordResetConfirmView.as_view()
+4 - Password successfully changed message     //PasswordResetCompleteView.as_view()
+'''
